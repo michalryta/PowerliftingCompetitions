80b29cbe03579d0bd90e8df647f7b4ff009ed7b2
PawelN31 <72617984+PawelN31@users.noreply.github.com>
2022-02-27 16:10:37 +0100

    Participants

--- app/src/main/java/com/bigboys/powerliftingcompetitions/Participants_Activity.java
+++ app/src/main/java/com/bigboys/powerliftingcompetitions/Participants_Activity.java
@@ -4,12 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Participants_Activity extends AppCompatActivity {
+import android.text.InputType;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.Button;
 
+import java.util.ArrayList;
+
+public class Participants_Activity extends AppCompatActivity {
+    Spinner sex;
+    EditText weight,name,surname;
+    Button buttonAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participants);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sex = (Spinner) findViewById(R.id.spinnerSex);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Sex");
+        list.add("Male");
+        list.add("Female");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, list);
+
+        sex.setAdapter(adapter);
+
+        name = (EditText) findViewById(R.id.TextEditName);
+        surname = (EditText) findViewById(R.id.TextEditSurname);
+
+        weight = (EditText) findViewById(R.id.TextEditWeight);
+        weight.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!name.equals("") && !surname.equals("")  &&
+                !weight.equals("") && !sex.getSelectedItem().equals("Sex")){
+                    //tutaj wysylamy do api dane
+                }
+                name.setText("");
+                surname.setText("");
+                weight.setText("");
+                sex.setSelection(0);
+            }
+        });
     }
 }
