dff0192936c20e5ac472f25b4aaa682c9cbcbdf1
michalryta <72621805+michalryta@users.noreply.github.com>
2022-03-05 18:09:29 +0100

    test

--- app/src/main/java/com/bigboys/powerliftingcompetitions/Participants_Activity.java
+++ app/src/main/java/com/bigboys/powerliftingcompetitions/Participants_Activity.java
@@ -11,12 +11,25 @@ import android.widget.Spinner;
 import android.widget.EditText;
 import android.widget.Button;
 
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Participants_Activity extends AppCompatActivity {
     Spinner sex;
     EditText weight,name,surname;
     Button buttonAdd;
+    //////////
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,8 +58,41 @@ public class Participants_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(!name.equals("") && !surname.equals("")  &&
-                !weight.equals("") && !sex.getSelectedItem().equals("Sex")){
+                !weight.equals("") && !sex.getSelectedItem().equals("Sex")) {
+
+
+                    try {
+                        http url = new URL("localhost:8080/athletes");
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                        con.setRequestMethod("POST");
+
+                        DataOutputStream wr= new DataOutputStream(
+                                con.getOutputStream());
+
+                        HashMap values = new HashMap<String, String>() {{
+                            put("name",name.getText().toString());
+                            put("surname",surname.getText().toString());
+                            put("pesel","00000");
+                            put("weight",weight.getText().toString());
+                            put("sex",sex.getSelectedItem().toString());
+                        }};
+////////////////////////////////////////////////////////////////////////////////////
+//url.set
+                        //wr.writeBytes("sss","");
+
+
+                        con.disconnect();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+
+                    }
+
+
+
+
                     //tutaj wysylamy do api dane
+
+
                 }
                 name.setText("");
                 surname.setText("");
